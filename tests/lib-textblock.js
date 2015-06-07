ab77829584bf31eceb3a108e84631220195c1a6f
Ken Wronkiewicz <wh@wirewd.com>
2015-06-06 17:25:21 -0700

    refactored unit tests, added validation of JSON payloads.

--- tests/lib-textblock.js
+++ tests/lib-textblock.js
@@ -1,88 +1,196 @@
 var textblock = require('../lib/textblock');
 var should = require('chai').should();
 
+// Zalgo haunts the internet seeking evil, danger, and people who parse XHTML with 
+// regexes.
+var zalgo = "\u005A\u0340\u035A\u0318\u0061\u035C\u0329\u0318\u0339\u0320\u006C\u035E\u0332\u0067\u0315\u0324\u0348\u033C\u0324\u031E\u006F\u0338\u0349\u0325";
+// A former coworker of mine accidentally broke a bunch of stuff with the script A.
+var awesome = "\uD835\uDC9C\u0077\u0065\u0073\u006F\u006D\u0065";
+
+
 describe('text_block', function() {
-	/**
-	 * Test that we can input 'atxplaintext' and that it turns four sections
-	 * into individual text blocks
-	 */
-	it('should make atxplaintext', function () {
-		var input = '# head\n\nblah\nblah bla#h\n# head2\n\nblah2\n# head3\n## head4';
-		var block = textblock.makeTextBlock(input,'atxplaintext');
-		block.format.should.equal('section');
-		block.blocks.length.should.equal(4);
-		block.blocks.forEach(function(element) {
-			element.format.should.equal('html');
-		});
-		var str = textblock.outputTextBlock(block);
-		str.should.equal('<h1>head</h1><p>blah\nblah bla#h\n</p><h1>head2</h1><p>blah2\n</p><h1>head3</h1><p></p><h2>head4</h2><p></p>');
-	});
-
-
-	/**
-	 * Test that we can input markdown and that it stores the source 
-	 * and also that it formats it properly
-	 */
-	it('should make markdown', function () {
+	describe('with markdown', function() {
 		var input = '# head\n\nblah\nblah bla#h\n\n# head2\n\nblah2\n\n# head3\n\n## head4';
-		var block = textblock.makeTextBlock(input,'markdown');
-		block.format.should.equal('markdown');
-		block.should.have.property('source');
-		block.should.have.property('htmltext');
-		var str = textblock.outputTextBlock(block);
-		str.should.equal('<h1>head</h1>\n\n<p>blah\nblah bla#h</p>\n\n<h1>head2</h1>\n\n<p>blah2</p>\n\n<h1>head3</h1>\n\n<h2>head4</h2>');
-	});
+		/**
+		 * Test that we can input markdown and that it stores the source 
+		 * and also that it formats it properly
+		 */
+		it('#makeTextBlock should work', function () {
+			var block = textblock.makeTextBlock(input,'markdown');
+			block.format.should.equal('markdown');
+			block.should.have.property('source');
+			block.should.have.property('htmltext');
+		});
 
-	/*
-	 * Ensures that we accept 'plainishtext' and that it gets encoded
-	 * as HTML properly
-	 */ 
-	it('should make plainishtext', function () {
-		var block = textblock.makeTextBlock('&blah\n\nblah','plainishtext');
-		var str = textblock.outputTextBlock(block);
-		str.should.equal("<p>&amp;blah</p>\n<p>blah</p>");
-	});
+		it('#outputTextBlock should work', function () {
+			var block = textblock.makeTextBlock(input,'markdown');
+			var str = textblock.outputTextBlock(block);
+			str.should.equal('<h1>head</h1>\n\n<p>blah\nblah bla#h</p>\n\n<h1>head2</h1>\n\n<p>blah2</p>\n\n<h1>head3</h1>\n\n<h2>head4</h2>');
+		});
 
-	/*
-	 * Ensures that html is a passthrough
-	 */ 
-	it('should make html', function () {
-		var block = textblock.makeTextBlock('<div>Test</div>','html');
-		var str = textblock.outputTextBlock(block);
-		str.should.equal("<div>Test</div>");
-	});
-
-	/*
-	 * Tests that we can make a text block and then make sections out of it.
-	 */
-	it('should make sections', function () {
-		var block = textblock.makeTextBlock('&blah\n\nblah','plainishtext');
-		var blocks = textblock.makeTextBlockSection(block);
-		var str = textblock.outputTextBlock(block);
-		str.should.equal("<p>&amp;blah</p>\n<p>blah</p>");
-	});
-
-	/*
-	 * Tests that unicode hilarity is preserved.
-	 */
-	it('should preserve zalgo unicode hilarity', function() {
-
-		// Zalgo haunts the internet seeking evil, danger, and people who parse XHTML with 
-		// regexes.
-		var zalgo = "\u005A\u0340\u035A\u0318\u0061\u035C\u0329\u0318\u0339\u0320\u006C\u035E\u0332\u0067\u0315\u0324\u0348\u033C\u0324\u031E\u006F\u0338\u0349\u0325";
-		// A former coworker of mine accidentally broke a bunch of stuff with the script A.
-		var awesome = "\uD835\uDC9C\u0077\u0065\u0073\u006F\u006D\u0065";
+		it('should handle zalgo unicode weirdness', function() {
+			var block = textblock.makeTextBlock(zalgo + '\n\n' + awesome,'markdown');
+			var str = textblock.outputTextBlock(block);
+			str.should.equal("<p>" + zalgo + "</p>\n\n<p>" + awesome + "</p>");
+		});
 		
-		var block = textblock.makeTextBlock('<div>'+ zalgo + awesome + '</div>','html');
-		var str = textblock.outputTextBlock(block);
-		str.should.equal('<div>'+ zalgo + awesome + '</div>');
+		describe('#validateTextBlock', function() {
+			it ('should work', function() {
+				var inputblock = {format: 'markdown', source: input};
+				var block = textblock.validateTextBlock(inputblock);
+				block.should.have.property('format');
+				block.format.should.equal('markdown');
+				block.should.have.property('source');
+				block.should.have.property('htmltext');
+			});
 
-		block = textblock.makeTextBlock(zalgo + '\n\n' + awesome,'plainishtext');
-		str = textblock.outputTextBlock(block);
-		str.should.equal("<p>" + zalgo + "</p>\n<p>" + awesome + "</p>");
+			it ('should remove bad keys', function() {
+				var inputblock = {format: 'markdown', htmltext: 'fer',
+					 gonzo: 'ponies', source: '# get'};
+				var block = textblock.validateTextBlock(inputblock);
+				block.should.have.property('format');
+				block.format.should.equal('markdown');
+				block.should.have.property('source');
+				block.should.not.have.property('gonzo');
+				block.should.have.property('htmltext');
+				block.htmltext.should.equal('<h1>get</h1>');
+			});
+		});
+	});
 
-		block = textblock.makeTextBlock(zalgo + '\n\n' + awesome,'markdown');
-		str = textblock.outputTextBlock(block);
-		str.should.equal("<p>" + zalgo + "</p>\n\n<p>" + awesome + "</p>");
+	describe('with html', function() {
+		/*
+		 * Ensures that html is a passthrough
+		 */ 
+		it('#makeTextBlock and #outputTextBlock should be a passthrough', function () {
+			var block = textblock.makeTextBlock('<div>Test</div>','html');
+			var str = textblock.outputTextBlock(block);
+			str.should.equal("<div>Test</div>");
+		});
+
+		it('should preserve zalgo unicode hilarity', function() {
+			var block = textblock.makeTextBlock('<div>'+ zalgo + awesome + '</div>','html');
+			var str = textblock.outputTextBlock(block);
+			str.should.equal('<div>'+ zalgo + awesome + '</div>');
+		});
+
+		describe('#validateTextBlock', function() {
+			it ('should work', function() {
+				var inputblock = {format: 'html', source: '<div>Test</div>'};
+				var block = textblock.validateTextBlock(inputblock);
+				block.should.have.property('format');
+				block.format.should.equal('html');
+				block.should.not.have.property('source');
+				block.should.have.property('htmltext');
+			});
+
+			it ('should remove bad keys', function() {
+				var inputblock = {format: 'html',
+					 gonzo: 'ponies', source: '<div>Test</div>'};
+				var block = textblock.validateTextBlock(inputblock);
+				block.should.have.property('format');
+				block.format.should.equal('html');
+				block.should.not.have.property('source');
+				block.should.not.have.property('gonzo');
+				block.should.have.property('htmltext');
+				block.htmltext.should.equal('<div>Test</div>');
+			});
+
+			it ('should also accept htmltext', function() {
+				var inputblock = {format: 'html',
+					 htmltext: '<div>Test</div>'};
+				var block = textblock.validateTextBlock(inputblock);
+				block.should.have.property('format');
+				block.format.should.equal('html');
+				block.should.not.have.property('source');
+				block.should.not.have.property('gonzo');
+				block.should.have.property('htmltext');
+				block.htmltext.should.equal('<div>Test</div>');
+			});
+		});
+	});
+
+	describe('with atxplaintext', function() {
+		/**
+		 * Test that we can input 'atxplaintext' and that it turns four sections
+		 * into individual text blocks
+		 */
+		it('should work', function () {
+			var input = '# head\n\nblah\nblah bla#h\n# head2\n\nblah2\n# head3\n## head4';
+			var block = textblock.makeTextBlock(input,'atxplaintext');
+			block.format.should.equal('section');
+			block.blocks.length.should.equal(4);
+			block.blocks.forEach(function(element) {
+				element.format.should.equal('html');
+			});
+			var str = textblock.outputTextBlock(block);
+			str.should.equal('<h1>head</h1><p>blah\nblah bla#h\n</p><h1>head2</h1><p>blah2\n</p><h1>head3</h1><p></p><h2>head4</h2><p></p>');
+		});
+
+		describe('#validateTextBlock', function() {
+			it ('should work', function() {
+				var inputblock = {format: 'plainishtext', source: 'twt'};
+				var block = textblock.validateTextBlock(inputblock);
+				block.should.have.property('format');
+				block.format.should.equal('plainishtext');
+				block.should.have.property('source');
+			});
+		});
+	});
+
+	describe('with plainishtext', function() {
+		/*
+		 * Ensures that we accept 'plainishtext' and that it gets encoded
+		 * as HTML properly
+		 */ 
+		it('should work', function () {
+			var block = textblock.makeTextBlock('&blah\n\nblah','plainishtext');
+			var str = textblock.outputTextBlock(block);
+			str.should.equal("<p>&amp;blah</p>\n<p>blah</p>");
+		});
+
+		/*
+		 * Tests that unicode hilarity is preserved.
+		 */
+		it('should preserve zalgo unicode hilarity', function() {
+			var block = textblock.makeTextBlock(zalgo + '\n\n' + awesome,'plainishtext');
+			var str = textblock.outputTextBlock(block);
+			str.should.equal("<p>" + zalgo + "</p>\n<p>" + awesome + "</p>");
+		});
+	});
+
+	describe('with sections', function() {
+		/*
+		 * Tests that we can make a text block and then make sections out of it.
+		 */
+		it('should make sections', function () {
+			var block = textblock.makeTextBlock('&blah\n\nblah','plainishtext');
+			var blocks = textblock.makeTextBlockSection(block);
+			var str = textblock.outputTextBlock(block);
+			str.should.equal("<p>&amp;blah</p>\n<p>blah</p>");
+		});
+
+		describe('#validateTextBlock', function() {
+			it ('should work', function() {
+				var inputblock = {format: 'section', 
+					maxnum: 134,
+					blocks: [
+						{format: 'plainishtext', source: 'blah blah'},
+						{format: 'html', source: '<h1>blah</h1>', mf: 'et'}
+					]
+				};
+				var block = textblock.validateTextBlock(inputblock);
+				block.should.have.property('format');
+				block.format.should.equal('section');
+				block.should.not.have.property('source');
+				block.should.not.have.property('htmltext');
+				block.should.not.have.property('maxnum');
+				block.blocks[0].should.have.property('format');
+				block.blocks[0].should.not.have.property('htmltext');
+				block.blocks[0].should.have.property('source');
+				block.blocks[1].should.have.property('format');
+				block.blocks[1].should.have.property('htmltext');
+			});
+		});
 	});
 });
